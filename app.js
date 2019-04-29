85b58c172a2ed8a2a427d942e8503ceb3e6a381d
Kevin Alwell <alwell-kevin@github.com>
2019-04-29 14:56:42 -0400

    spin node express server

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var Wizard = require("./Wizard.js");
+require('dotenv').config()
+var express = require('express');
+var bodyParser = require('body-parser');
+
 
 var wizard = new Wizard();
 
@@ -9,3 +13,16 @@ wizard.castSpell();
 wizard.warmUp("BooooM");
 
 wizard.castSpell();
+
+app.all("/middle-earth", (req, res) => {
+    console.log("************************************")
+    console.log("middle-earth REQ: ", req.body);
+    console.log("************************************")
+
+    res.sendStatus(200)
+})
+
+// Start server
+app.listen(port, () => {
+    console.log('Express server started on port ' + port);
+})
