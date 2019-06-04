93d22550ce9b456077be5779ded93c6ddab1e5f5
Kevin Alwell <alwell-kevin@github.com>
2019-06-03 20:15:37 -0400

    remove unnecessary server

--- app.js
+++ app.js
@@ -13,16 +13,3 @@ wizard.castSpell();
 wizard.warmUp("BooooM");
 
 wizard.castSpell();
-
-app.all("/middle-earth", (req, res) => {
-    console.log("************************************")
-    console.log("middle-earth REQ: ", req.body);
-    console.log("************************************")
-
-    res.sendStatus(200)
-})
-
-// Start server
-app.listen(port, () => {
-    console.log('Express server started on port ' + port);
-})
